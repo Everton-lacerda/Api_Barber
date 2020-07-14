dfe1d08359ce0a30dc110d5d53589410d2367fda
Everton Lacerda <everton3b@hotmail.com>
2020-07-13 21:20:11 -0300

    add eslint

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,9 @@
-import express from 'express'
+import express from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => {
-    return res.json({message: 'Funcionando'})
-})
+app.get('/', (req, res) => res.json({ message: 'Funcionando' }));
 
 app.listen(3333, () => {
-    console.log('Server starter on port 3333!')
-})
+  console.log('Server starter on port 3333!');
+});
